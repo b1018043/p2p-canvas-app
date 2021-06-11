25feaf37cae3feb37eddf9ee786fd19e18244781
b1018043 <grey.fox.0417@gmail.com>
2021-06-11 12:42:45 +0900

    mod:webpack5の利用に切り替え

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,21 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname,'dist')
     },
+    resolve:{
+        fallback:{
+            "fs": false,
+            "tls": false,
+            "net": false,
+            "path": false,
+            "zlib": false,
+            "http": false,
+            "https": false,
+            "stream": false,
+            "crypto": false,
+            "assert": false,
+            "os": false
+        }
+    },
     plugins:[
         new webpack.ProvidePlugin({
             Buffer: ['buffer', 'Buffer'],
